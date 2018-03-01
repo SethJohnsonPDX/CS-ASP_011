586a827e40cea01b1bb88a7fb3b7e54391da7eb4
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-02-28 20:45:50 -0800

    commit all

---
+++ CS-ASP_011/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_011.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            Is
+            <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+&nbsp;equal to
+            <asp:TextBox ID="secondTextBox" runat="server"></asp:TextBox>
+            ?</p>
+        <p>
+            <asp:CheckBox ID="coolCheckBox" runat="server" Text="Are you cool?" />
+        </p>
+        <p>
+            If you could only eat one food for the rest of your life which one would you choose?</p>
+        <p>
+            <asp:RadioButton ID="pizzaRadioButton" runat="server" Checked="True" GroupName="foodGroup" Text="Pizza" />
+        </p>
+        <p>
+            <asp:RadioButton ID="saladRadioButton" runat="server" GroupName="foodGroup" Text="Salad" />
+        </p>
+        <p>
+            <asp:RadioButton ID="sandwichRadioButton" runat="server" GroupName="foodGroup" Text="PBJ Sandwich" />
+        </p>
+        <p>
+            <asp:Button ID="okButton" runat="server" OnClick="Button1_Click" Text="Ok" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_011/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_011
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+
+            /* if (firstTextBox.Text == secondTextBox.Text)
+             {
+                 resultLabel.Text = "Yes! They're equal!";
+             }
+             else
+             {
+                 resultLabel.Text = "No. They're not equal.";
+             }
+
+             if (coolCheckBox.Checked)
+             {
+                 resultLabel.Text = "Yes, you're definitely cool!";
+             }
+             else
+             {
+                 resultLabel.Text = "Don't be so hard on yourself.";
+             }
+             */
+
+            if (pizzaRadioButton.Checked)
+            {
+                resultLabel.Text = "You must be from Chicago!";
+            }
+            else if (saladRadioButton.Checked)
+            {
+                resultLabel.Text = "You must be healthy!";
+            }
+            else if (sandwichRadioButton.Checked)
+            {
+                resultLabel.Text = "You must be a fun loving person!";
+            }
+            else
+            {
+                resultLabel.Text = "Please select one of the options.";
+            }
+        }
+    }
+}
